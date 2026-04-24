ae2bf7eac30ebe8ec45649df3c7dd780dce14e80
Abhi <mitecollege123@gmail.com>
2026-04-24 21:25:46 +0530

    first push

--- voltride/src/data/constants.js
+++
@@ -1,3 +0,0 @@
-/** User reference point (Koramangala, Bengaluru) */
-export const USER_LAT = 12.9352;
-export const USER_LNG = 77.6245;
--- voltride/src/utils/stationFilters.js
+++
@@ -1,4 +0,0 @@
-export function filterStations(stations, showAvailableOnly) {
-  if (!showAvailableOnly) return [...stations];
-  return stations.filter((s) => s.status === "available");
-}
